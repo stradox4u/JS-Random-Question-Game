84606c5a39d1b8269320d6656756e04f9edb71a1
stradox4u <stradox@gmail.com>
2018-10-15 15:51:13 +0100

    second attempt

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
 
+//IIFE Wrapping
 (function() {
 //Question Function Constructor
 var Question = function(question, answers, correctAnswer) {
@@ -7,39 +8,52 @@ var Question = function(question, answers, correctAnswer) {
     this.correctAnswer = correctAnswer
 };
 
-//Random Question Selector
-    var n = Math.floor(Math.random() * 3);
-
 //Question Display Method
 Question.prototype.displayQuestion = function() {
-    console.log(questions[n].question);
-    for (i = 0; i < questions[n].answers.length; i++) {
-            console.log(i + ':' + questions[n].answers[i]);
+    console.log(this.question);
+    for (i = 0; i < this.answers.length; i++) {
+            console.log(i + ':' + this.answers[i]);
             }
 }
     
-
+var initialScore = 0;
 //Correct Answer Checker Method
 Question.prototype.checkAnswer = function() {
     var userAnswer = prompt('Please enter the correct answer here (Enter the number only)');
     if (parseInt(userAnswer) === this.correctAnswer) {
         console.log('That is the correct answer!');
+        console.log('------------------------------');
     } else {
         console.log('That is a wrong answer. Please try again!');
+        console.log('------------------------------');
+
     }
+    if(userAnswer !== 'exit') {
+        nextQuestion();
+    }
+       
 }
 
 //Questions
 var q1 = new Question('What is the world\'s biggest mass market luxury car brand?', ['Audi', 'BMW', 'Mercedes Benz', 'Bentley'], 2);
 
-var q2 = new Question('Which is Formula One\'s most popular team?', ['Red Bull', 'Williams', 'Mercedes Benz', 'Ferrari', 'Force India'], 3);
+var q2 = new Question('Which is Formula One\'s most popular team?', ['Red Bull', 'Williams', 'Mercedes Benz', 'Force India', 'Ferrari'], 4);
 
-var q3 = new Question('Which Formula One driver is the most successful of all time?', ['Lewis Hamilton', 'Ayrton Senna', 'Felipe Massa', 'Sebastian Vettel', 'Michael Schumacher', 'Juan Manuel Fangio', 'Jackie Stewart'], 4);
+var q3 = new Question('Which Formula One driver is the most successful of all time?', ['Lewis Hamilton', 'Ayrton Senna', 'Sebastian Vettel', 'Michael Schumacher', 'Juan Manuel Fangio', 'Jackie Stewart'], 3);
 
 var questions = [q1, q2, q3];
 
+
+var n = Math.floor(Math.random() * questions.length);
 questions[n].displayQuestion();
 questions[n].checkAnswer();
-}) ();
 
+function nextQuestion() {
+    var qNext = Math.floor(Math.random() * questions.length);
+    questions[qNext].displayQuestion();
+    questions[qNext].checkAnswer();
+}
+
+
+}) ();
 
